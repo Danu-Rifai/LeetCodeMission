a6b96b5d9d58aa9fcd6d3502bd1ec3f8360448b2
Danurifai <rifaidanu12@gmail.com>
2026-04-03 23:39:45 +0700

    Time: 21 ms (38.35%), Space: 26 MB (40.11%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,16 +1,10 @@
 class Solution(object):
   def containsDuplicate(self, nums):
     mySet = set()
-    duplikat = set()
     for i in nums:
       if i not in mySet:
         mySet.add(i)
       else:
-        duplikat.add(i)
-      
-    if duplikat:
-      return True
-    else:
-      return False
+        return True
     
-    return status
+    return False
